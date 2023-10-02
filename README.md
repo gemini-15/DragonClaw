eb7ce86d632363e7ef20e64563c8c916c8e2b078
Yass <bargach.y@gmail.com>
2023-10-02 15:36:13 -0400

    readme update

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 DragonClaw aims to answer to the tedious process of getting a usable reverse shell from the worst machine possible. 
 
-This came up as a first idea of having to redo the same things over and over again (which is the definition of insanity). 
+This came up as an first after having to redo the same things over and over again (which is the definition of insanity). 
 
 ## When it will be available ? 
 
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from rich import print
 
 from connection_handler.server import Server
 
-
-def main(ip: str = "0.0.0.0", port: int = 4444):
-    print("\n[dark_red]██████  ██████   █████   ██████   ██████  ███    ██  ██████ ██       █████  ██     ██ \n\
+BANNER_DRAGONCLAW = "\n[dark_red] \
+██████  ██████   █████   ██████   ██████  ███    ██  ██████ ██       █████  ██     ██ \n\
 ██   ██ ██   ██ ██   ██ ██       ██    ██ ████   ██ ██      ██      ██   ██ ██     ██ \n\
 ██   ██ ██████  ███████ ██   ███ ██    ██ ██ ██  ██ ██      ██      ███████ ██  █  ██ \n\
 ██   ██ ██   ██ ██   ██ ██    ██ ██    ██ ██  ██ ██ ██      ██      ██   ██ ██ ███ ██ \n\
-██████  ██   ██ ██   ██  ██████   ██████  ██   ████  ██████ ███████ ██   ██  ███ ███[/dark_red]")
+██████  ██   ██ ██   ██  ██████   ██████  ██   ████  ██████ ███████ ██   ██  ███ ███[/dark_red]"
     
+
+def main(ip: str = "0.0.0.0", port: int = 4444):
+    print(BANNER_DRAGONCLAW)
     loop = asyncio.get_event_loop()
     server = Server(loop, ip, port)
-    
+
 if __name__ == "__main__":
     typer.run(main)
